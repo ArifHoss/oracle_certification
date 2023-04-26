1cf1608329c4ffac16f05ca7ae9de8ac769b818c
Arif Hossain <hossaininfo@ymail.com>
2023-04-26 14:30:43 +0200

    ImportantStringBuilderMethod

--- src/com/example/understanding_strings/ImportantStringBuilderMethod.java
+++ src/com/example/understanding_strings/ImportantStringBuilderMethod.java
@@ -12,10 +12,43 @@ public class ImportantStringBuilderMethod {
         char ch = sb.charAt(5); // a
         System.out.println(sub + " " + len + " " + ch);
 
-        // append(), insert(), delete(), deleteCharAt(), reverse(), toString()
+        // append()
         StringBuilder b = new StringBuilder().append(1).append("L");
         b.append("-").append(true);
 
+        //insert()
+        StringBuilder sb1 = new StringBuilder("animals");
+        sb1.insert(7, "-"); // animals-
+        sb1.insert(2, 2); // a2nimals-
+        System.out.println(sb1); // a2nimals-
+
+        //delete() and deleteCharAt()
+
+        StringBuilder sb2 = new StringBuilder("abcdef");
+        sb2.delete(1, 3); // acdef
+        sb2.delete(1,1); // acdef // no change
+        System.out.println(sb2);// acdef
+        sb2.deleteCharAt(5); // acde
+        System.out.println(sb2);// acde
+
+        //reverse()
+        StringBuilder sb3 = new StringBuilder("ABC");
+        sb3.reverse(); // CBA
+        System.out.println(sb3); // CBA
+
+        //toString()
+        String toString = sb3.toString();// CBA
+        System.out.println(toString); // CBA
+
+        //capacity()
+        StringBuilder sb4 = new StringBuilder();
+        System.out.println(sb4.capacity()); // 16
+        sb4.append("Hello World");
+        System.out.println(sb4.capacity()); // 16
+        sb4.append("Hello World");
+        System.out.println(sb4.capacity()); // 34
+        StringBuilder sb5 = new StringBuilder(100);
+        System.out.println(sb5.capacity()); // 100
 
     }
 }
---
+++ src/com/example/understanding_strings/UnderstandingEquality.java
@@ -0,0 +1,23 @@
+package com.example.understanding_strings;
+
+public class UnderstandingEquality {
+    public static void main(String[] args) {
+        StringBuilder one = new StringBuilder();
+        StringBuilder two = new StringBuilder();
+        StringBuilder three = one.append("Java");
+        System.out.println(one == two); // false
+        System.out.println(one == three); // true
+
+        String x = "Java";
+        String y = "Java";
+        System.out.println(x == y); // true
+
+        String a = "Java";
+        String b = " Java".trim();
+        System.out.println(a == b); // false
+
+        String c = "Java";
+        String d = " Ja".trim() + "va"; // trim() returns a new string
+        System.out.println(c == d); // false
+    }
+}
